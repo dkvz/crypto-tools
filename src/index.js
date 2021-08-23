a439bcce7bdcc1ae049844cd4a45e57eb45438c8
DkVZ <github@dkvz.eu>
2021-08-23 14:38:15 +0200

    Add loading modal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 require('./styles/styles.pcss')
 
-console.log('Coucou')
+const [cryptForm, clearBtn, encryptRadio, loadingModal] = [
+  'cryptForm', 'clearBtn', 'encryptRadio', 'loadingModal'
+].map(e => document.getElementById(e))
+
+function displayLoading(show) {
+  if (show) {
+    document.body.setAttribute('data-overlay', true)
+    scrollTo(0, 0);
+    loadingModal.classList.remove('hidden')
+  } else {
+    document.body.removeAttribute('data-overlay')
+    loadingModal.classList.add('hidden')
+  }
+}
+
+cryptForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  displayLoading(true)
+})
