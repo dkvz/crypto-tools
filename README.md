a963a4a51faa1ee255afae71eec28b545a59f6c9
DkVZ <github@dkvz.eu>
2021-08-23 15:02:17 +0200

    More scaffolding

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Client-side JavaScript crypto tools, mostly something to encode text in AES.
 
 ## Resources
 - Got a pen with a complete key derivation: https://codepen.io/dkvz/pen/MMdbey
+
+# TODO
+- [ ] Add a webroot folder with a favicon.
--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 require('./styles/styles.pcss')
 
-const [cryptForm, clearBtn, encryptRadio, loadingModal] = [
-  'cryptForm', 'clearBtn', 'encryptRadio', 'loadingModal'
+const warnClass = 'text-yellow-500'
+
+const [
+  cryptForm,
+  clearBtn,
+  encryptRadio,
+  loadingModal,
+  msg,
+  payloadText,
+  passwordInput
+] = [
+  'cryptForm',
+  'clearBtn',
+  'encryptRadio',
+  'loadingModal',
+  'msg',
+  'payloadText',
+  'passwordInput'
 ].map(e => document.getElementById(e))
 
 function displayLoading(show) {
   if (show) {
     document.body.setAttribute('data-overlay', true)
-    scrollTo(0, 0);
+    scrollTo(0, 0)
     loadingModal.classList.remove('hidden')
   } else {
     document.body.removeAttribute('data-overlay')
@@ -15,7 +31,25 @@ function displayLoading(show) {
   }
 }
 
+function showMsg(message, warning = true) {
+  if (message) {
+    msg.textContent = message
+    warning && msg.classList.add(warnClass)
+    msg.classList.remove('hidden')
+    msg.scrollIntoView()
+  } else {
+    clearMsg()
+  }
+}
+
+function clearMsg() {
+  msg.classList.add('hidden')
+  msg.classList.remove(warnClass)
+}
+
 cryptForm.addEventListener('submit', (e) => {
   e.preventDefault()
   displayLoading(true)
+  clearMsg()
+
 })
